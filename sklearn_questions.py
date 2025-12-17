d372df4eaa6f7f91a09ff395bf19f9ac76850149
olivierfontaine14 <olivier.fontaine@hec.edu>
2025-12-17 16:57:19 +0100

    Up my solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -62,6 +62,7 @@ from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.utils.multiclass import check_classification_targets
 
 
+
 class KNearestNeighbors(ClassifierMixin, BaseEstimator):
     """KNearestNeighbors classifier."""
 
