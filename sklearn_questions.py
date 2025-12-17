dae0374caafaef64e7217799ef9b287bdb8ce15f
olivierfontaine14 <olivier.fontaine@hec.edu>
2025-12-17 16:52:45 +0100

    Up my solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -207,7 +207,6 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
         if self.time_col == "index":
             if not isinstance(X.index, pd.DatetimeIndex):
                 raise ValueError(
